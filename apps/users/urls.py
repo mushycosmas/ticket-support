170e75f495117a3de212d63d8769616a41c992c3
mushycosma <kevomassawe@gmail.com>
2026-07-17 13:01:41 +0300

    fix

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,19 @@
+# apps/users/urls.py
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, TeamViewSet, LoginView
+
+from .views import UserViewSet, TeamViewSet
+from .views.auth_views import (
+    LoginView,
+    RefreshTokenView,
+    VerifyTokenView,
+    LogoutView,
+    MeView,
+)
 from .views.profile_views import ChangePasswordView
+
+
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'teams', TeamViewSet, basename='team')
@@ -9,5 +21,9 @@ router.register(r'teams', TeamViewSet, basename='team')
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', LoginView.as_view(), name='login'),
-    path("change-password/", ChangePasswordView.as_view(), name="change-password"),
+    path('me/', MeView.as_view(), name='me'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('refresh/', RefreshTokenView.as_view(), name='refresh-token'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('verify-token/', VerifyTokenView.as_view(), name='verify-token'),
 ]
